f2bf892c31fed4568737c57f98cfdc5404c004ec
TeunvanDinther <tinusteun12345@gmail.com>
2020-04-05 12:53:12 +0200

    range verbeterd

--- oopg-master/src/main/java/tankGame/Tanks.java
+++ oopg-master/src/main/java/tankGame/Tanks.java
@@ -25,7 +25,7 @@ public class Tanks extends SpriteObject implements ICollidableWithTiles {
 	int selectedType = 0;
 	ArrayList<Ammo> ammo = new ArrayList<>();
 
-	Tanks(Main app, String sprite, int player,float xPos,float yPos) {
+	Tanks(Main app, String sprite, int player, float xPos, float yPos) {
 
 		super(new Sprite(Main.MEDIA_URL.concat(sprite)));
 
@@ -41,7 +41,7 @@ public class Tanks extends SpriteObject implements ICollidableWithTiles {
 	@Override
 	public void update() {
 		checkHealth();
-	
+
 	}
 
 	@Override
@@ -49,12 +49,12 @@ public class Tanks extends SpriteObject implements ICollidableWithTiles {
 		if (keyCode == app.LEFT && player == 1 && app.player1Turn == true
 				|| key == 'a' && player == 2 && app.player1Turn == false) {
 			setDirectionSpeed(270, 5);
-			 app.player1Turn = ! app.player1Turn;
+			app.player1Turn = !app.player1Turn;
 		}
 		if (keyCode == app.RIGHT && player == 1 && app.player1Turn == true
 				|| key == 'd' && player == 2 && app.player1Turn == false) {
 			setDirectionSpeed(270, -5);
-			 app.player1Turn = ! app.player1Turn;
+			app.player1Turn = !app.player1Turn;
 		}
 
 	}
@@ -70,100 +70,85 @@ public class Tanks extends SpriteObject implements ICollidableWithTiles {
 	}
 
 	void fire() {
-
-		if (player == 1) {
-			if (getAngleFrom(app.mouseX, app.mouseY) > 270 || getAngleFrom(app.mouseX, app.mouseY) < 90) {
-				if (getDistance() < 100 && getDistance() > 20) {
-					Ammo a = new Ammo(getAngleFrom(app.mouseX, app.mouseY) + randomNumber(-10, 10), selectedType, 1, app, "bullet.png",
-							this.getDistance() / 20,this);
-					ammo.add(a);
-					app.addGameObject(a, super.x, super.y);
-
-				}
+		float range = 0;
+		if (getAngleFrom(app.mouseX, app.mouseY) > 270 || getAngleFrom(app.mouseX, app.mouseY) < 90) {
+			if (getDistance() < 100 && getDistance() > 20) {
+				range = getDistance();
 			}
-
-		}
-		if (player == 2) {
-			if (getAngleFrom(app.mouseX, app.mouseY) > 270 || getAngleFrom(app.mouseX, app.mouseY) < 90) {
-				if (getDistance() < 100 && getDistance() > 20) {
-					Ammo a = new Ammo(getAngleFrom(app.mouseX, app.mouseY) + randomNumber(-10, 10), selectedType, 1, app, "bullet.png", this.getDistance() / 20,this);
-					ammo.add(a);
-					app.addGameObject(a, super.x, super.y);
-				}
+			else if (getDistance() <= 20) {
+				range = 20;
 			}
+			else if(getDistance() >= 100) {
+				range = 100;
+			}
+				Ammo a = new Ammo(getAngleFrom(app.mouseX, app.mouseY) + randomNumber(-10, 10), selectedType, 1, app,
+						"bullet.png", range / 20, this);
+				ammo.add(a);
+				app.addGameObject(a, super.x, super.y);
 
+			
 		}
-
 	}
-	
+
 	void damage(float damageDone) {
 		this.health -= damageDone;
 		System.out.println(this.health);
-		
+
 	}
+
 	void checkHealth() {
-		if(health <= 0) {
-			if(player == 1) {
-			app.addScorePlayer2(1);
-			
+		if (health <= 0) {
+			if (player == 1) {
+				app.addScorePlayer2(1);
+
 			}
-			if(player == 2) {
+			if (player == 2) {
 				app.addScorePlayer1(1);
-				}
+			}
 			this.health = 100;
 		}
-		
-		
-		
+
 	}
-	
-	 @Override
-		public void tileCollisionOccurred(List<CollidedTile> collidedTiles) {
 
-			PVector vector;
-			for (CollidedTile ct : collidedTiles) {
-				if (ct.getTile() instanceof groundTiles) {
+	@Override
+	public void tileCollisionOccurred(List<CollidedTile> collidedTiles) {
 
-					try {
+		PVector vector;
+		for (CollidedTile ct : collidedTiles) {
+			if (ct.getTile() instanceof groundTiles) {
 
-						
-						if (ct.getCollisionSide() == CollisionSide.RIGHT) {
-							vector = app.getTileMap().getTilePixelLocation(ct.getTile());
-							this.setX(vector.x + this.getWidth());
-						}
-						if (ct.getCollisionSide() == CollisionSide.TOP) {
-							vector = app.getTileMap().getTilePixelLocation(ct.getTile());
-							this.setY(vector.y - this.getHeight());
-							
-						
-						}
-						if (ct.getCollisionSide() == CollisionSide.LEFT) {
-							vector = app.getTileMap().getTilePixelLocation(ct.getTile());
-							this.setX(vector.x - this.getWidth());
-						}
+				try {
 
-					} catch (TileNotFoundException e) {
-						e.printStackTrace();
+					if (ct.getCollisionSide() == CollisionSide.RIGHT) {
+						vector = app.getTileMap().getTilePixelLocation(ct.getTile());
+						this.setX(vector.x + this.getWidth());
+					}
+					if (ct.getCollisionSide() == CollisionSide.TOP) {
+						vector = app.getTileMap().getTilePixelLocation(ct.getTile());
+						this.setY(vector.y - this.getHeight());
 
 					}
+					if (ct.getCollisionSide() == CollisionSide.LEFT) {
+						vector = app.getTileMap().getTilePixelLocation(ct.getTile());
+						this.setX(vector.x - this.getWidth());
+					}
+
+				} catch (TileNotFoundException e) {
+					e.printStackTrace();
+
 				}
 			}
-
 		}
-	
 
-
-	
-
-	
+	}
 
 	private float getDistance() {
 		float d = app.dist(super.x, super.y, app.mouseX, app.mouseY);
 		return d;
 	}
-	
+
 	private float randomNumber(float min, float max) {
-		float randomNum =(float) Math.random() * (max - min + 1) + min;
+		float randomNum = (float) Math.random() * (max - min + 1) + min;
 		return randomNum;
 	}
 
