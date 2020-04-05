21f8331794a804b084114149c0d63902edb540b6
TeunvanDinther <tinusteun12345@gmail.com>
2020-04-05 16:04:21 +0200

    userinterface

---
+++ oopg-master/src/main/java/tankGame/UserInterface.java
@@ -0,0 +1,26 @@
+package tankGame;
+
+import nl.han.ica.oopg.objects.TextObject;
+
+public class UserInterface extends TextObject {
+	int health = 100;
+	Main app;
+	String name;
+
+	public UserInterface(String text, int fontSize, Main app, String name) {
+		super(text, fontSize);
+		this.setForeColor(255, 255, 255, 255);
+		this.app = app;
+		this.name = name;
+	}
+
+	public void displayHealth(Tanks player) {
+		setText(name +Integer.toString(player.health));
+		
+	}
+
+	public void update() {
+		this.displayHealth(app.getPlayer1());
+		this.displayHealth(app.getPlayer2());
+	}
+}
